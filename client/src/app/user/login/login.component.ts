25e1c4237a56ae601ad612863ae721ae54703ab7
Godwin Agedah <agedah99@gmail.com>
2020-03-27 23:14:50 +0100

    google login

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -63,7 +63,7 @@ export class LoginComponent implements OnInit {
 
   log() {
 
-    window.open('/api/auth/google', '_self');
+    window.open('https://ngchatapp.herokuapp.com/api/auth/google', '_self');
     // window.open('/api/auth/google',"mywindow","location=1,status=1,scrollbars=1, width=800,height=800");
     /*let listener = window.addEventListener('message', (message) => {
       console.log(message);
